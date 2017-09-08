b9bbd330f1879e80c517e8ef97e7a9069dd3cd10
BaoHan HP <hbao@wpi.edu>
2017-09-08 00:16:40 -0400

    problem 01 create

---
+++ problem-01.py
@@ -0,0 +1,47 @@
+import json
+
+import twitter
+
+
+# ---------------------------------------------
+# Define a Function to Login Twitter API
+def oauth_login():
+    # Go to http://twitter.com/apps/new to create an app and get values
+    # for these credentials that you'll need to provide in place of these
+    # empty string values that are defined as placeholders.
+    # See https://dev.twitter.com/docs/auth/oauth for more information
+    # on Twitter's OAuth implementation.
+
+    CONSUMER_KEY = 'THNn3A8S9iEVVoxwujipnwvnt'
+    CONSUMER_SECRET = 'cyBdLmR8GpychGE69Co1x1Uw1sgsrs00Yyeye8YipuUBtw4Fsq'
+    OAUTH_TOKEN = '1625211115-DgaXEatHXAUOrzKwlPXiwgOFDKA28JoA3xDcx4T'
+    OAUTH_TOKEN_SECRET = '5FL2PVPnIz6fRVuQpQdENsGiMusFgQqqMB3XcvOvDv7uN'
+
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                               CONSUMER_KEY, CONSUMER_SECRET)
+
+    twitter_api = twitter.Twitter(auth=auth)
+    return twitter_api
+
+
+# Define a function to save data in a file
+def save_to_file(file_name, data):
+    with open(file_name, mode='w') as f:
+        f.write(json.dumps(data, indent=2))
+
+
+# Define a function to save at most max_num of tweets in some topic to a file
+def save_topic_to_file(topic, max_num, file_name):
+    twitter_stream = twitter.TwitterStream(auth=oauth_login().auth)
+    filtered_stream = twitter_stream.statuses.filter(track=topic, language='en')
+    data = []
+    for tweet in filtered_stream:
+        data.append(tweet)
+        max_num -= 1
+        if max_num < 0:
+            break
+    save_to_file(file_name, data)
+
+
+topic = 'Donald Trump,Barack Obama'
+save_topic_to_file(topic, 10, 'problem-1.json')
